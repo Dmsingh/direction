06d3f6ad24176f0024082114726c23b979ff9278
Dmsingh <dm.singh723dm@gmail.com>
2022-02-20 12:47:47 +0530

    last commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
+
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
@@ -17,7 +18,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Navbar/>
     </div>
   );
 }
